85fbf95c727314aac53d9dcf83ab92e15fb48201
Mustafa DAĞTEKİN <mustafa.dagtekin@gmail.com>
2019-12-26 01:19:10 +0300

    Kata1 -PrimeFactors

--- PrimeFactors/Form1.Designer.cs
+++ PrimeFactors/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(120, 75);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(123, 42);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxNumber.Location = new System.Drawing.Point(73, 49);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(225, 20);
+            this.textBoxNumber.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter a Value";
+            // 
+            // labelResult
+            // 
+            this.labelResult.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelResult.Location = new System.Drawing.Point(70, 134);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(228, 86);
+            this.labelResult.TabIndex = 3;
+            this.labelResult.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(399, 229);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prime Factors";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelResult;
     }
 }
 
--- PrimeFactors/Form1.cs
+++ PrimeFactors/Form1.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PrimeFactors
+
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public int number;
+        public const int primeNumber=2;
+       // primes = new List<int>();
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+
+            number = Convert.ToInt32(textBoxNumber.Text);
+           
+            if (number%2==0)
+            {
+                
+            }
+            //MessageBox.Show(number.ToString());
         }
     }
 }
