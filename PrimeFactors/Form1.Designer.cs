4171d55ddd7a5a1b06add901d9c9fc5a7d1ebc05
Mustafa DAĞTEKİN <mustafa.dagtekin@gmail.com>
2019-12-31 00:36:52 +0300

    PrimeFactors

--- PrimeFactors/Form1.Designer.cs
+++ PrimeFactors/Form1.Designer.cs
@@ -31,16 +31,17 @@
             this.buttonStart = new System.Windows.Forms.Button();
             this.textBoxNumber = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonAddToPrimeList = new System.Windows.Forms.Button();
+            this.listBoxPrimes = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(120, 75);
+            this.buttonStart.Location = new System.Drawing.Point(73, 75);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(123, 42);
+            this.buttonStart.Size = new System.Drawing.Size(86, 32);
             this.buttonStart.TabIndex = 0;
-            this.buttonStart.Text = "Start";
+            this.buttonStart.Text = "Prime Or Not ?";
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
@@ -61,21 +62,31 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Enter a Value";
             // 
-            // labelResult
+            // buttonAddToPrimeList
             // 
-            this.labelResult.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.labelResult.Location = new System.Drawing.Point(70, 134);
-            this.labelResult.Name = "labelResult";
-            this.labelResult.Size = new System.Drawing.Size(228, 86);
-            this.labelResult.TabIndex = 3;
-            this.labelResult.Text = "label2";
+            this.buttonAddToPrimeList.Location = new System.Drawing.Point(212, 75);
+            this.buttonAddToPrimeList.Name = "buttonAddToPrimeList";
+            this.buttonAddToPrimeList.Size = new System.Drawing.Size(86, 46);
+            this.buttonAddToPrimeList.TabIndex = 4;
+            this.buttonAddToPrimeList.Text = "Find Prime And Add To List";
+            this.buttonAddToPrimeList.UseVisualStyleBackColor = true;
+            this.buttonAddToPrimeList.Click += new System.EventHandler(this.buttonAddToPrimeList_Click);
+            // 
+            // listBoxPrimes
+            // 
+            this.listBoxPrimes.FormattingEnabled = true;
+            this.listBoxPrimes.Location = new System.Drawing.Point(73, 122);
+            this.listBoxPrimes.Name = "listBoxPrimes";
+            this.listBoxPrimes.Size = new System.Drawing.Size(225, 95);
+            this.listBoxPrimes.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(399, 229);
-            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.listBoxPrimes);
+            this.Controls.Add(this.buttonAddToPrimeList);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxNumber);
             this.Controls.Add(this.buttonStart);
@@ -92,7 +103,8 @@
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.TextBox textBoxNumber;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonAddToPrimeList;
+        private System.Windows.Forms.ListBox listBoxPrimes;
     }
 }
 
--- PrimeFactors/Form1.cs
+++ PrimeFactors/Form1.cs
@@ -13,8 +13,11 @@ namespace PrimeFactors
         }
 
         public int number;
-        public const int primeNumber=2;
-       // primes = new List<int>();
+        public int counter;
+        public int primeNumber;
+        public int control;
+
+        // primes = new List<int>();
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
@@ -23,9 +26,38 @@ namespace PrimeFactors
            
             if (number%2==0)
             {
-                
+                MessageBox.Show("Girdiğiniz Sayı Asal Değildir!");
+            }
+            else
+            {
+                MessageBox.Show("Girdiğiniz Sayı Asaldır!");
             }
             //MessageBox.Show(number.ToString());
+        }
+
+        private void buttonAddToPrimeList_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            number = Convert.ToInt32(textBoxNumber.Text);
+            for (primeNumber=2; primeNumber  < number; primeNumber++)
+            {
+                control = 0;
+                if (number%2==0)
+                {
+                    control = 1;
+                    break;
+                }
+
+            }
+            if (control==1)
+            {
+                listBoxPrimes.Items.Add(number + " asal değildir !");
+            }
+            else
+            {
+                listBoxPrimes.Items.Add(number + " asaldır !");
+                counter++;
+            }
         }
     }
 }
